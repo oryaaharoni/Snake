c05d984669342f7680e15d98954bf8db163b6bbd
oryaaharoni <oryaaharonii@gmail.com>
2024-02-13 18:05:56 +0200

    snake move and delete other frames from the canvas

--- snake.py
+++ snake.py
@@ -42,9 +42,41 @@ window.geometry(f"{window_width}x{window_width}+{window_x}+{window_y}")
 snake = Tile(5 * TILE_SIZE, 5 * TILE_SIZE) #single tile for the snake's head
 food = Tile(10 * TILE_SIZE, 10 * TILE_SIZE)
 
-def draw():
+# velocity- the change of the snake every time- event
+velocityX = 0
+velocityY = 0
+
+def change_direction(e):   #e= event
+    # print(e)      
+    # print(e.keysym) #wich key i pressed
+    global velocityX, velocityY
+
+    if (e.keysym=="Up"):
+        velocityX = 0
+        velocityY = -1
+    elif (e.keysym=="Down"):
+        velocityX = 0
+        velocityY = 1
+    elif (e.keysym=="Left"):
+        velocityX = -1
+        velocityY = 0
+    elif (e.keysym=="Right"):
+        velocityX = 1
+        velocityY = 0
+
+
+def move():
     global snake
 
+    snake.x += velocityX * TILE_SIZE
+    snake.y += velocityY * TILE_SIZE
+
+
+def draw():
+    global snake
+    move()
+
+    canvas.delete("all")  #clear the other frame
     # draw snake
     # fill- the color on the snake, and the other is the position top, botton, left , right
     canvas.create_rectangle(snake.x, snake.y, snake.x + TILE_SIZE, snake.y + TILE_SIZE, fill = "lime green" )
@@ -58,4 +90,6 @@ def draw():
 
 draw()
 
+# key listener- every time the key click the func is called
+window.bind("<KeyRelease>", change_direction)
 window.mainloop()
