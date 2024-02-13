aecfc1091a636db5a573af21ad13aba3bb1add05
oryaaharoni <oryaaharonii@gmail.com>
2024-02-13 17:46:54 +0200

    draw the food on canvas

--- snake.py
+++ snake.py
@@ -40,6 +40,7 @@ window.geometry(f"{window_width}x{window_width}+{window_x}+{window_y}")
 
 # initial game 
 snake = Tile(5 * TILE_SIZE, 5 * TILE_SIZE) #single tile for the snake's head
+food = Tile(10 * TILE_SIZE, 10 * TILE_SIZE)
 
 def draw():
     global snake
@@ -47,6 +48,12 @@ def draw():
     # draw snake
     # fill- the color on the snake, and the other is the position top, botton, left , right
     canvas.create_rectangle(snake.x, snake.y, snake.x + TILE_SIZE, snake.y + TILE_SIZE, fill = "lime green" )
+    
+    # draw the food
+    canvas.create_rectangle(food.x, food.y, food.x + TILE_SIZE, food.y + TILE_SIZE , fill = 'red')
+
+
+
     window.after(100, draw)  #after 100 millseconds i will call draw() again
 
 draw()
