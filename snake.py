53392eec7cd55a0d024954587032f8df6388fdc9
oryaaharoni <oryaaharonii@gmail.com>
2024-02-13 17:44:35 +0200

    draw snake on canvas

--- snake.py
+++ snake.py
@@ -8,18 +8,47 @@ TILE_SIZE = 25
 WINDOW_WIDTH = TILE_SIZE * ROWS
 WINDOW_HEIGHT = TILE_SIZE * COLS
 
+class Tile:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
 
 # game window
 window = tkinter.Tk()
 window.title("snake")
-# we dont want that the user expend the size of the window
-# hieght-false, width- false
+# we dont want that the user expend the size of the window hieght-false, width- false
 window.resizable(False, False)
 
 # canvas to draw
-# canvas- master prop is window, backgroundColor is black, height and width
-canvas = tkinter.Canvas(window, bg = "black", width = WINDOW_WIDTH, height = WINDOW_HEIGHT)
+# canvas- master prop is window, backgroundColor is black, height and width , borderwidth and highlightthickness remove the white border
+canvas = tkinter.Canvas(window, bg = "black", width = WINDOW_WIDTH, height = WINDOW_HEIGHT, borderwidth = 0, highlightthickness = 0)
 canvas.pack()
 window.update()
+
+# center the window on our computer screen
+window_width = window.winfo_width()
+window_height = window.winfo_height()
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+
+window_x = int((screen_width/2) - (window_width/2))
+window_y = int((screen_height/2) - (window_height/2))
+
+# format string+objects
+window.geometry(f"{window_width}x{window_width}+{window_x}+{window_y}")
+
+# initial game 
+snake = Tile(5 * TILE_SIZE, 5 * TILE_SIZE) #single tile for the snake's head
+
+def draw():
+    global snake
+
+    # draw snake
+    # fill- the color on the snake, and the other is the position top, botton, left , right
+    canvas.create_rectangle(snake.x, snake.y, snake.x + TILE_SIZE, snake.y + TILE_SIZE, fill = "lime green" )
+    window.after(100, draw)  #after 100 millseconds i will call draw() again
+
+draw()
 
 window.mainloop()
